50e64f024c6c84687d1592782402350df47f378d
TarasVolianskyi <volianskyitv@gmail.com>
2017-10-02 21:18:26 +0300

    Update functions of buttons

--- app/src/main/java/com/volianskyi/taras/a300917_cv/MainActivity.java
+++ app/src/main/java/com/volianskyi/taras/a300917_cv/MainActivity.java
@@ -1,7 +1,10 @@
 package com.volianskyi.taras.a300917_cv;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clickGoToMyLocation(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:50.4546600,30.5238000"));
+        startActivity(intent);
+    }
+
+    public void clickGoToMyEmail (View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
+        startActivity(intent);
+    }
+
+    public void clickGoToMyPhoneNumber(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:+380974526583"));
+        startActivity(intent);
+    }
+
 }
