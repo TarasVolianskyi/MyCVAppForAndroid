fc56f570c114c1475ad35f5c5276b3894cf9bf78
TarasVolianskyi <volianskyitv@gmail.com>
2017-10-07 20:19:44 +0300

    Update structure

--- app/src/main/java/com/volianskyi/taras/a300917_cv/SkillsActivity.java
+++ app/src/main/java/com/volianskyi/taras/a300917_cv/SkillsActivity.java
@@ -1,7 +1,11 @@
 package com.volianskyi.taras.a300917_cv;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SkillsActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class SkillsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skills);
         getSupportActionBar().setTitle("Skills");
+      //  onClickBtnGoToListFromThisPage();
     }
+/*
+    private void onClickBtnGoToListFromThisPage() {
+        Button btnGoToMainList = (Button) findViewById(R.id.btnListOfCategories);
+        btnGoToMainList.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+      //  Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(SkillsActivity.this, CategoriesActivity.class);
+        startActivity(intent);
+    }*/
 }
